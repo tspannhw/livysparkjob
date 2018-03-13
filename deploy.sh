e56a2fff802495d841d289edb0b743195278acd9
Timothy Spann <tspann@hortonworks.com>
2018-03-13 10:58:01 -0400

    Clean up

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,4 @@
-scp target/scala-2.11/example_2.11-1.0.jar centos@princeton0.field.hortonworks.com:/opt/demo
+mv target/scala-2.11/example_2.11-1.0.jar target/scala-2.11/example.jar
+scp target/scala-2.11/example.jar centos@princeton0.field.hortonworks.com:/opt/demo
+scp target/scala-2.11/example.jar centos@princeton1.field.hortonworks.com:/opt/demo
+
--- src/main/scala-2.11/Example.scala
+++ src/main/scala-2.11/Example.scala
@@ -1,30 +1,14 @@
 package com.dataflowdeveloper.example
 
-import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.serializer.{KryoSerializer}
-import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
-import org.apache.spark.rdd.RDD
-import org.apache.spark.serializer.{KryoSerializer, KryoRegistrator}
-import org.apache.log4j.Logger
-import org.apache.log4j.Level
 import org.apache.spark.sql.SparkSession
 
-object Links {
+class Example () {
 
-  //// Main Spark Program
-  def main(args: Array[String]) {
-    Logger.getLogger("com.dataflowdeveloper.links").setLevel(Level.INFO)
-
-    val log = Logger.getLogger("com.dataflowdeveloper.example")
-    log.info("Started Example")
-
-    val spark = SparkSession
-      .builder
-      .appName("Example")
-      .getOrCreate()
+  def run( spark: SparkSession) { 
 
       try {
+        println("Started")
+
 	val shdf = spark.read.json("hdfs://princeton0.field.hortonworks.com:8020/spark2-history")
  
 	shdf.printSchema()
@@ -38,7 +22,6 @@ object Links {
         println("Complete.")
       } catch {
         case e: Exception =>
-          log.error("Writing files after job. Exception:" + e.getMessage);
           e.printStackTrace();
       }
   }
