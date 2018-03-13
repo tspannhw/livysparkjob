7ac25977757bdf6b5a828f8123f9e528f8796fed
Timothy Spann <tspann@hortonworks.com>
2018-03-13 11:03:51 -0400

    Change logs

--- src/main/scala-2.11/Example.scala
+++ src/main/scala-2.11/Example.scala
@@ -9,13 +9,13 @@ class Example () {
       try {
         println("Started")
 
-	val shdf = spark.read.json("hdfs://princeton0.field.hortonworks.com:8020/spark2-history")
+	val shdf = spark.read.json("hdfs://princeton0.field.hortonworks.com:8020/smartPlugRaw")
  
 	shdf.printSchema()
  
-	shdf.createOrReplaceTempView("sparklogs")
+	shdf.createOrReplaceTempView("smartplug")
  
-	val stuffdf = spark.sql("SELECT * FROM sparklogs")
+	val stuffdf = spark.sql("SELECT * FROM smartplug")
  
 	stuffdf.count()
 
